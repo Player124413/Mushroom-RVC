8941f18679f2b143d3780f713151c35c48c5b6a4
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-07-26 13:22:41 +0300

    Update f0.py

--- rvc/lib/predictors/f0.py
+++ rvc/lib/predictors/f0.py
@@ -5,7 +5,7 @@ import torch
 import torchcrepe
 from torchfcpe import spawn_bundled_infer_model
 
-from rvc.lib.predictors.RMVPE import RMVPEF0Predictor
+from rvc.lib.predictors.RMVPEV2 import RMVPE0PredictorV2
 
 
 def median_interp_pitch(f0):
@@ -88,7 +88,7 @@ class RMVPE:
     def __init__(self, device, sample_rate=16000):
         self.device = device
         self.sample_rate = sample_rate
-        self.model = RMVPEF0Predictor(os.path.join("rvc", "models", "predictors", "rmvpe.pt"), device=self.device)
+        self.model = RMVPE0PredictorV2(os.path.join("rvc", "models", "predictors", "rmvpe.pt"), device=self.device)
 
     def get_f0(self, audio, type_rmvpe="rmvpe"):
         if type_rmvpe == "rmvpe":
